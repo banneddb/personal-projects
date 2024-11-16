5f0e6948cd1f72da3360076aac44c696756fee40
Behruz Omon <behruzomon6@gmail.com>
2024-11-16 18:45:52 -0500

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,22 @@
+#CALCULATOR:
+num1 = int(input("First number:"))
+operation = input("Pick from the following: +, -, *, /")
+num2 = int(input("Second number:"))
+
+def calculator(num1, operation, num2):
+    if operation == "+":
+        sum = num1+num2
+        print("Your product is", sum)
+    elif operation == "-":
+        subtract = num1-num2
+        print(subtract)
+    elif operation == "*":
+        product = num1*num2
+        print(product)
+    elif operation == "/":
+        quotient = num1/num2
+        print(quotient)
+    else:
+        print("That is an invalid operation!")
+
+calculator(num1, operation, num2)
